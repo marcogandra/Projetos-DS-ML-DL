7dbca2d318e0406088d36999f4d9e966c80e79fe
Olavo Mendes <olavo.77@hotmail.com>
2020-06-09 21:24:24 -0300

    update comentarios codigo

--- Deep Learning/batmanXsupermanClassifier/batmanXsuperman.py
+++ Deep Learning/batmanXsupermanClassifier/batmanXsuperman.py
@@ -13,7 +13,7 @@ import numpy as np
 class Interface():
     
     # Função para selecionar e carregador no programa a imagem que será classificada
-    def Captura(self):
+    def iapture(self):
      
         self.filename = askopenfilename()
 
@@ -25,31 +25,31 @@ class Interface():
         label.image = self.photo
 
     # Função para treinar a rede neural com as imagens da pasta batmanXsuperman
-    def Treinamento(self):
+    def train(self):
         
         # Criando o modelo da rede
-        self.classificador = Sequential()
+        self.classifier = Sequential()
         
         # Adicionando as convoluções
-        self.classificador.add(Conv2D(64, (3, 3), input_shape=(64, 64, 3), activation='relu'))
-        self.classificador.add(BatchNormalization())
-        self.classificador.add(MaxPooling2D(pool_size=(2, 2)))
+        self.classifier.add(Conv2D(64, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+        self.classifier.add(BatchNormalization())
+        self.classifier.add(MaxPooling2D(pool_size=(2, 2)))
         
         # Adicionando a camada de flatten
-        self.classificador.add(Flatten())
+        self.classifier.add(Flatten())
 
         # Criando a rede neural densa
-        self.classificador.add(Dense(units=128, activation='relu'))
-        self.classificador.add(Dense(units=128, activation='relu'))
+        self.classifier.add(Dense(units=128, activation='relu'))
+        self.classifier.add(Dense(units=128, activation='relu'))
         # Criando a camada de saída
-        self.classificador.add(Dense(units=1, activation='sigmoid'))
+        self.classifier.add(Dense(units=1, activation='sigmoid'))
 
         # Compilando a rede neural
-        self.classificador.compile(optimizer='adam', loss='binary_crossentropy',
+        self.classifier.compile(optimizer='adam', loss='binary_crossentropy',
                               metrics=['accuracy'])
         
         # Criando um gerador de imagens para treinamento
-        gerador_treinamento = ImageDataGenerator(rescale=1. / 255,
+        train_data_gen = ImageDataGenerator(rescale=1. / 255,
                                                  rotation_range=7,
                                                  horizontal_flip=True,
                                                  shear_range=0.2,
@@ -57,10 +57,10 @@ class Interface():
                                                  zoom_range=0.2)
 
         # Criando um gerador de imagens para teste
-        gerador_teste = ImageDataGenerator(rescale=1. / 255)
+         test_data_gen = ImageDataGenerator(rescale=1. / 255)
 
         # Usando o gerador de imagens de treinamento para criar a base de treinamento
-        base_treinamento = gerador_treinamento.flow_from_directory('batmanXsuperman/training_set',
+        train_base = train_data_gen.flow_from_directory('batmanXsuperman/training_set',
                                                             target_size = (64,64),
                                                             batch_size = 32,
                                                             class_mode = 'binary')
@@ -68,57 +68,57 @@ class Interface():
     
     
         # Usando o gerador de imagens de teste para criar a base de teste
-        base_teste = gerador_treinamento.flow_from_directory('batmanXsuperman/test_set',
+        test_base = test_data_gen.flow_from_directory('batmanXsuperman/test_set',
                                                             target_size = (64,64),
                                                             batch_size = 32,
                                                             class_mode = 'binary')
         
         # Treinando o classificador. Não utilizei muitas épocas devido a capacidade de processamento 
         # do meu PC não ser tão grande
-        self.classificador.fit_generator(base_treinamento, steps_per_epoch=5000/32,
-                                    epochs=3, validation_data=base_teste,
+        self.classifier.fit_generator(train_base, steps_per_epoch=5000/32,
+                                    epochs=3, validation_data=test_base,
                                     validation_steps=370/32)
 
-        self.contador = 1
+        self.counter = 1
 
     # Função para classificar a imagem
-    def ClassificarImagens(self):
+    def classifyImage(self):
         
         # Carregando a imagem em uma variável
-        imagem_teste = image.load_img(self.filename,
+        test_image = image.load_img(self.filename,
                                    target_size = (64, 64))
           
         # Transformando a imagem em array 
-        imagem_teste = image.img_to_array(imagem_teste)
+        test_image = image.img_to_array(test_image)
         # Deixando o array na escala entre 0 e 1
-        imagem_teste /= 255
+        test_image /= 255
         
         # Expandindo a dimensão no array para ser aceitável no classificador
-        imagem_teste = np.expand_dims(imagem_teste, axis= 0)
+        test_image = np.expand_dims(imagem_teste, axis= 0)
         
         # Usando o classificador para prever a imagem
-        previsao = self.classificador.predict(imagem_teste)
+        pred = self.classificador.predict(test_image)
         
         # Se o classificador prever 0 (e já que 0 é menor que 0.5) é uma imagem do Batman
-        if previsao <= 0.5:
+        if pred <= 0.5:
             print('A imagem é do Batman')
             Label(self.root, text='É O BATMAN').grid(row=1, column=0)
             
         # Se o classificador prever 1 (e já que 1 é maior que 0.5) é uma imagem do Superman
-        elif previsao >=0.5:
+        elif pred >=0.5:
             print('A imagem é do Superman')
             Label(self.root, text='É O SUPERMAN').grid(row=1, column=0)
             
 
     def __init__(self):
-        self.contador = 0
+        self.counter = 0
         self.root = Tk()
         self.root.resizable(True, True)
         self.root.protocol("WM_DELETE_WINDOW")
         self.root.title("Identificaçao de imagens")
         
         # Criando o botão para selecionar uma imagem
-        Button(self.root,text='selecione a imagem', command=self.Captura).grid(row=0, column=0, pady=5)
+        Button(self.root,text='Selecione a imagem', command=self.Captura).grid(row=0, column=0, pady=5)
         
         # Criando o botão para treinar a rede
         Button(self.root, text='Treinar Rede', command=self.Treinamento, width=10, height=2).grid(row=0, column=1)
